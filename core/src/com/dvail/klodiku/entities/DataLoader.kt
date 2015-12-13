e9d721c591a6733d18d04d0c42e9495394f94991
David Vail <dcvail17@gmail.com>
2015-12-12 20:20:46 -0500

    Get TOML loader into a workable, somewhat coherent state.

--- core/src/com/dvail/klodiku/entities/DataLoader.kt
+++ core/src/com/dvail/klodiku/entities/DataLoader.kt
@@ -3,7 +3,6 @@ package com.dvail.klodiku.entities
 import com.badlogic.ashley.core.Component
 import com.badlogic.ashley.core.Engine
 import com.badlogic.ashley.core.Entity
-import com.dvail.klodiku.combat.getDefaultWeaponDamType
 import com.moandjiezana.toml.Toml
 import java.io.File
 import java.util.*
@@ -13,78 +12,83 @@ class DataLoader() {
     val compStringMap = HashMap<String, Class<out Component>>()
 
     init {
+        compStringMap.put("State", Comps.State)
+        compStringMap.put("MobAI", Comps.MobAI)
         compStringMap.put("Spatial", Comps.Spatial)
-        compStringMap.put("EqWeapon", Comps.EqWeapon)
         compStringMap.put("Renderable", Comps.Renderable)
+        compStringMap.put("AnimatedRenderable", Comps.AnimatedRenderable)
+        compStringMap.put("Item", Comps.Item)
+        compStringMap.put("EqItem", Comps.EqItem)
+        compStringMap.put("EqWeapon", Comps.EqWeapon)
+        compStringMap.put("EqArmor", Comps.EqArmor)
+        compStringMap.put("Equipment", Comps.Equipment)
+        compStringMap.put("Inventory", Comps.Inventory)
     }
 
     fun loadArea(world: Engine, areaName: String) {
         val areaData = File("./maps/$areaName/data.toml")
         val areaToml = Toml().read(areaData)
 
-      //  loadMobs(world, areaToml.getTables("mob"))
         loadFreeItems(world, areaToml.getTables("free-item"))
-    }
-
-    private fun loadMobs(world: Engine, mobs: List<Toml>) {
+        loadMobs(world, areaToml.getTables("mob"))
     }
 
     private fun loadFreeItems(world: Engine, items: List<Toml>) {
-        val itemComponentLists = items.map { loadComponents(it) }
+        val freeItemEntities = buildEntities(items)
+        freeItemEntities.forEach { world.addEntity(it) }
+    }
 
-        itemComponentLists.forEach { it ->
+    private fun loadMobs(world: Engine, mobs: List<Toml>) {
+        mobs.forEach { mob ->
+            val mobComps = buildComponentList(mob.getTable("components"))
+            val mobInventory = mob.getList<Toml>("inventory").map { buildComponentList(it) }
+            val mobEquipment = buildMobEq(mob.getTable("equipment"))
+
+            val mobEntity = Entity()
+            mobComps.forEach { mobEntity.add(it) }
+
+            val items = mobInventory.map { comps ->
+                val item = Entity()
+                comps.forEach { comp -> item.add(comp) }
+                item
+            }
+
+            items.forEach { item ->
+                world.addEntity(item)
+                CompMapper.Inventory.get(mobEntity).items.add(item)
+            }
+
+            mobEquipment.values.forEach { world.addEntity(it) }
+            CompMapper.Equipment.get(mobEntity).items = mobEquipment
+
+            world.addEntity(mobEntity)
+        }
+    }
+
+    private fun buildEntities(items: List<Toml>): List<Entity> {
+        val itemComponentLists = items.map { buildComponentList(it.getTable("components")) }
+
+        return itemComponentLists.map { it ->
             val item = Entity()
             it.forEach { comp -> item.add(comp) }
-            world.addEntity(item)
+            item
         }
     }
 
-    private fun loadComponents(item: Toml) : List<Component> {
-        val compTable = item.getTable("components")
-        val components = ArrayList<Component>()
-
-        compStringMap.keys.forEach { key ->
-            if (compTable.containsTable(key)) {
-                val comp = createComponent(compStringMap[key], compTable.getTable(key))
-                if (comp != null) components.add(comp)
-            }
-        }
-
-        return components
+    private fun buildComponentList(table: Toml) : List<Component> {
+        return table.entrySet().map { createComponent(compStringMap[it.key], it.value as Toml) }
     }
 
-    private fun loadMobEq(toml: Toml) : HashMap<EqSlot, List<Component>> {
-        val mobEq = HashMap<EqSlot, List<Component>>()
+    private fun buildMobEq(toml: Toml) : HashMap<EqSlot, Entity> {
+        val mobEq = HashMap<EqSlot, Entity>()
 
-        EqSlot.values().forEach { slot ->
-            val comp = toml.getTable(slot.toString().toLowerCase())
+        toml.entrySet().forEach { entry ->
+            val entity = Entity()
+            buildComponentList(entry.value as Toml).forEach { entity.add(it) }
+            mobEq.put(EqSlot.valueOf(entry.key), entity)
         }
 
         return mobEq
     }
 
-    /*
-    * This is awful...
-    */
-    private fun createComponent(clazz: Class<out Component>?, toml: Toml) : Component? {
-        when (clazz) {
-            Comps.Spatial -> {
-                if (toml.containsPrimitive("pos") && toml.getString("pos").equals("Carried")) {
-                    return Spatial(Carried)
-                } else {
-                    return Spatial(toml.getDouble("x").toFloat(), toml.getDouble("y").toFloat(), toml.getDouble("radius").toFloat())
-                }
-            }
-            Comps.EqWeapon -> {
-                val weaponType = toml.getString("type")
-                val damType = getDefaultWeaponDamType(weaponType)
-                return EqWeapon(damType = damType, baseDamage = toml.getLong("baseDamage").toInt(), size = toml.getDouble("size").toFloat())
-            }
-            Comps.Renderable -> {
-                return Renderable(toml.getString("textureSource"))
-            }
-            else -> { return null }
-        }
-    }
 }
-
